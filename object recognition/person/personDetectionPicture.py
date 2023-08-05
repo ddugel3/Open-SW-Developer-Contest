fc69c6949e04d1a247241013ab1e8b88f818b319
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2023-08-05 19:26:39 +0900

    Docs: modify personDetectionPicture

--- object recognition/person/personDetectionPicture.py
+++ object recognition/person/personDetectionPicture.py
@@ -2,17 +2,17 @@ import cv2
 import numpy as np
 
 # Yolo 로드
-net = cv2.dnn.readNet("test/yolov3-tiny.weights", "test/yolov3-tiny.cfg")
+net = cv2.dnn.readNet("yolov3.cfg", "yolov3.weights")
 classes = []
-with open("test/coco.names", "r") as f:
+with open("Open-SW-Developer-Contest/object recognition/person/coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 layer_names = net.getLayerNames()
-output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
 
 # 이미지 가져오기
-img = cv2.imread("sample.jpg")
+img = cv2.imread("Open-SW-Developer-Contest/object recognition/person/water3.jpg")
 img = cv2.resize(img, None, fx=0.4, fy=0.4)
 height, width, channels = img.shape
 

88048d694c88ffb77b448222721499df983aafcb
HeeNamgoong <skrndgml2@kookmin.ac.kr>
2023-09-06 01:15:12 +0900

    Feat: Realtime Map MJ

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,168 @@
+package com.example.myapplication;
+
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.skt.Tmap.TMapGpsManager;
+import com.skt.Tmap.TMapMarkerItem;
+import com.skt.Tmap.TMapPoint;
+import com.skt.Tmap.TMapView;
+
+import java.util.ArrayList;
+
+
+
+public class MainActivity extends AppCompatActivity
+        implements TMapGpsManager.onLocationChangedCallback {
+
+    private Context mContext = null;
+    private boolean m_bTrackingMode = true;
+
+    private TMapGpsManager tmapgps = null;
+    private TMapView tmapview = null;
+    private static String mApiKey = "앱키입력하기"; // 발급받은 appKey
+    private static int mMarkerID;
+
+    private LinearLayout Mapview;
+
+    private ArrayList<TMapPoint> m_tmapPoint = new ArrayList<TMapPoint>();
+    private ArrayList<String> mArrayMarkerID = new ArrayList<String>();
+    private ArrayList<MapPoint> m_mapPoint = new ArrayList<MapPoint>();
+
+    @Override
+    public void onLocationChange(Location location) {
+        if (m_bTrackingMode) {
+            tmapview.setLocationPoint(location.getLongitude(), location.getLatitude());
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContext = this;
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.mapview);
+        tmapview = new TMapView(this);
+        linearLayout.addView(tmapview);
+        tmapview.setSKTMapApiKey("DWBbu4DTzLQhxCoLQ5u88vLX1nK6qPh65eKgkcn8" );
+
+        addPoint();
+        showMarkerPoint();
+
+        /* 현재 보는 방향 */
+        tmapview.setCompassMode(true);
+
+        /* 현위치 아이콘표시 */
+        tmapview.setIconVisibility(true);
+
+        /* 줌레벨 */
+        tmapview.setZoomLevel(15);
+        tmapview.setMapType(TMapView.MAPTYPE_STANDARD);
+        tmapview.setLanguage(TMapView.LANGUAGE_KOREAN);
+
+        tmapgps = new TMapGpsManager(MainActivity.this);
+        tmapgps.setMinTime(1000);
+        tmapgps.setMinDistance(5);
+        //tmapgps.setProvider(NETWORK_PROVIDER);
+        tmapgps.setProvider(TMapGpsManager.NETWORK_PROVIDER); //연결된 인터넷으로 현 위치를 받습니다.
+        //실내일 때 유용합니다.
+        //tmapgps.setProvider(tmapgps.GPS_PROVIDER); //gps로 현 위치를 잡습니다.
+        tmapgps.OpenGps();
+
+        /*  화면중심을 단말의 현재위치로 이동 */
+        tmapview.setTrackingMode(true);
+        tmapview.setSightVisible(true);
+
+        // 풍선에서 우측 버튼 클릭시 할 행동입니다
+        tmapview.setOnCalloutRightButtonClickListener(new TMapView.OnCalloutRightButtonClickCallback()
+        {
+            @Override
+            public void onCalloutRightButton(TMapMarkerItem markerItem) {
+                Toast.makeText(MainActivity.this, "클릭", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void addPoint() { //여기에 핀을 꼽을 포인트들을 배열에 add해주세요!
+        // 강남 //
+        m_mapPoint.add(new MapPoint("강남", 37.510350, 127.066847));
+    }
+
+
+    public void showMarkerPoint() {// 마커 찍는거 빨간색 포인트.
+        for (int i = 0; i < m_mapPoint.size(); i++) {
+            TMapPoint point = new TMapPoint(m_mapPoint.get(i).getLatitude(),
+                    m_mapPoint.get(i).getLongitude());
+            TMapMarkerItem item1 = new TMapMarkerItem();
+            Bitmap bitmap = null;
+            bitmap = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.poi_dot);
+            //poi_dot은 지도에 꼽을 빨간 핀 이미지입니다
+
+            item1.setTMapPoint(point);
+            item1.setName(m_mapPoint.get(i).getName());
+            item1.setVisible(item1.VISIBLE);
+
+            item1.setIcon(bitmap);
+
+            bitmap = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.poi_dot);
+
+            // 풍선뷰 안의 항목에 글을 지정합니다.
+            item1.setCalloutTitle(m_mapPoint.get(i).getName());
+            item1.setCalloutSubTitle("서울");
+            item1.setCanShowCallout(true);
+            item1.setAutoCalloutVisible(true);
+
+            Bitmap bitmap_i = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.speaker);
+
+            item1.setCalloutRightButtonImage(bitmap_i);
+
+            String strID = String.format("pmarker%d", mMarkerID++);
+
+            tmapview.addMarkerItem(strID, item1);
+            mArrayMarkerID.add(strID);
+        }
+    }
+    private static final int PERMISSION_REQUEST_LOCATION = 1;
+
+    // 위치 권한을 요청하는 메서드
+    private void requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // 권한을 요청합니다.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_REQUEST_LOCATION);
+        }
+    }
+
+    // 권한 요청 결과 처리
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 권한이 허용되었을 때의 동작을 수행하세요.
+            } else {
+                // 권한이 거부되었을 때의 동작을 수행하세요.
+            }
+        }
+    }
+
+}
